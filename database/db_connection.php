dfcb528cfdaf07197fd895e0cb610971104c588d
Gilbert Elikplim Kukah <kwamegilbert1114@gmail.com>
2024-12-19 14:43:34 +0000

    Password fixed

--- database/db_connection.php
+++ database/db_connection.php
@@ -1,9 +1,9 @@
 <?php
 // Define the database credentials
-$host = 'sql307.infinityfree.com';       // The hostname of the database server
-$dbname = 'if0_36702081_church_db';     // The name of the database you are connecting to
-$username = 'if0_36702081';        // Your database username
-$password = '9h7DhdmRJq';            // Your database password
+$host = 'localhost';       // The hostname of the database server
+$dbname = 'church_db';     // The name of the database you are connecting to
+$username = 'root';        // Your database username
+$password = '';            // Your database password
 
 // Establish a connection to the MySQL database using MySQLi
 $conn = new mysqli($host, $username, $password, $dbname);
